b71059a613ec2c3c0973c9b1fb12aa190e95a984
elfrmkr <elfrmkr98@gmail.com>
2021-06-29 16:11:35 +0300

    added image card to style images, search output can be seen

--- src/components/App.js
+++ src/components/App.js
@@ -28,3 +28,5 @@ render(){
 };
 
 export default App;
+
+
--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,16 +1,18 @@
 /* eslint-disable array-callback-return */
 import React from 'react';
+import ImageCard from './imageCard';
+import './imageList.css';
 
 const ImageList = (props) => {
   const images =  props.images.map(
-        ({description,id, urls}) => 
+        (image) => 
 // eslint-disable-next-line jsx-a11y/alt-text
 {
 // assign the key on the root element that u r returning
     // eslint-disable-next-line jsx-a11y/alt-text
-return  <img alt = {description} key = {id} src = {urls.regular}/>});
+return  <ImageCard key = {image.id} image = {image} />});
     
-return <div> {images} </div>
+return <div className = "image-list"> {images} </div>
 };
 
 export default ImageList;
