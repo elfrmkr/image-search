73967a7f0d8ed23b9c24b2ddef72cf2192e5b82e
elfrmkr <elfrmkr98@gmail.com>
2021-06-29 10:44:08 +0300

    fetching data from unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './searchbar';
 
-const App = () => {
-    return (<div className = "ui container" style = {{marginTop: '10px'}}>
-        <SearchBar/>
-    </div>
-    )
+class App extends React.Component {
+
+state = {
+    images: []
+    };
+/* Making request to get search of photos,
+async request. you can also use promise .then but this is easier
+*/
+ onSearchSubmit = async (term) => {
+   const response = await axios.get('https://api.unsplash.com/search/photos', {  params: {query: term},
+       headers: {
+           Authorization: 'Client-ID _l8Ap2dXnt2zy3YXJI5uvmiUsJK-Swb3EY9N3Zjcn10'
+        } 
+    });
+    this.setState({images: response.data.results});
+}  
+    
+render(){    
+    return (
+    <div className = "ui container" style = {{marginTop: '10px'}}>
+        <SearchBar onSubmit = {this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
+    </div>)
+    }
 };
 
 export default App;
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -12,15 +12,26 @@ onChange is a property to handle event, specific name = {this.onInputChange} is 
 
 uncontrolled vs controlled component: uncontrolled component makes finding the value of an input harder cuz you need to reach out reactDom to reference that input. Having control make you able to see the input value from your state
 */ 
+
+/*
+In order to use the identifier "this" in a event handler method we need to apply binding with a constructor. Otherwise it becomes undefined. Or, you can transfrom the function into arrow function not to bind anything
+
+You can do the same thing inside the onSubmit of form(the similar approach is available with onChange)
+ */
+onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+};
+
 render() {
     return (<div className = "ui segment">
-        <form className = "ui-form">
+        <form  onSubmit = {this.onFormSubmit} className = "ui-form">
         <div className = "field">
             <label>Image Search  </label>
             <input 
             type = "text" 
             value = {this.state.term}
-            onChange = {(event) => this.setState({term: event.target.value})}/>
+            onChange = {(event) => this.setState({term: event.target.value.toLowerCase()})}/>
         </div>
         </form>
     </div>);
